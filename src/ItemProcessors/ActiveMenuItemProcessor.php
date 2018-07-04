367aac11689d8b3b469be17c505e56b6bc815ad6
adelf <adel.faiz@gmail.com>
2018-07-04 20:24:22 +0300

    activeParent filling

--- src/ItemProcessors/ActiveMenuItemProcessor.php
+++ src/ItemProcessors/ActiveMenuItemProcessor.php
@@ -14,15 +14,20 @@ final class ActiveMenuItemProcessor implements LaravelMenuItemProcessor
         if (array_key_exists('active', $item))
         {
             $item['active'] = $item['active']();
+
+            if($item['active'])
+            {
+                $this->lastActiveItem = $item;
+            }
         }
         else
         {
             $item['active'] = false;
-        }
 
-        if($item['active'])
-        {
-            $this->lastActiveItem = $item;
+            if(array_key_exists('items', $item) && $this->lastActiveItem !== null)
+            {
+                $item['activeParent'] = array_search($this->lastActiveItem, $item['items']) !== false;
+            }
         }
 
         return $item;
